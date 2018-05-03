dbd8dbc3e766d3031d689a927005b20b44d9b175
Ahmed <ahmedemam755@gmail.com>
2018-05-03 19:14:56 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+angular.module('ToDo', [])
+    .controller('todoController',['$scope', function($scope) {
+        $scope.todos = [
+            {'title': 'Learn Angular.JS',
+             'done': false
+            }
+        ];
+
+        $scope.addTodo = function(){
+            $scope.todos.push({'title':$scope.newTodo,'done':false})
+            $scope.newTodo = '';
+        }
+
+
+        $scope.clearCompleted = function(){
+            $scope.todos = $scope.todos.filter(function(item){
+                return !item.done
+            })
+        }
+    }]);
